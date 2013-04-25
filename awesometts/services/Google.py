56ff9003065b7b64861a6ccfb680f82a0027eea2
Paperfeed <paperfeed@gmail.com>
2013-04-25 15:05:31 +0300

    Fix GoogleTTS, change file generation
    With added delay

--- awesometts/services/Google.py
+++ awesometts/services/Google.py
@@ -50,12 +50,15 @@ slanguages = [['af', 'Afrikaans', 'cp1252'], #or iso-8859-1
 TTS_ADDRESS = 'http://translate.google.com/translate_tts'
 
 
-import re, subprocess, urllib
+import os, re, subprocess, urllib, cookielib, time
+from random import uniform
 from anki.utils import stripHTML
 from urllib import quote_plus
 import awesometts.config as config
 import awesometts.util as util
 from subprocess import Popen, PIPE, STDOUT
+import urllib2
+
 
 
 
@@ -114,18 +117,57 @@ def recordGoogleTTS(form, text):
 
 def TTS_record_old(text, language):
 	text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
-	url = TTS_ADDRESS+'?tl='+language+'&q="'+ text + '"'
+	url = TTS_ADDRESS+'?tl=' + language + '&q=' + text
+	cookie = 'PREF=ID=86407e90cde8b907:U=9484059a8a1d2bcd:FF=4:LR=lang_en|lang_nl:LD=en:CR=2:TM=1361915611:LM=1366208091:GM=1:SG=1:S=kcO-GQbhBXiiXu7V; NID=67=REkGJ-g0wzEy5QPIKD_cX9o3yUw9VXoHb-qyS8w6qyWYWq_oCGWJon5vJ8V5Yg6QDrXA1LOkPRaVTkrX_YX-6Bz_Y9glnILDCifTE57SRFlRBf1L32_F3ncDw5uleoiyfQtHDGXthPvXgyt6p_pJ3ITdZiC10oKGM7M8b5AAZMz_D1ESKmtKE3d7uXcc-vBy1m0F3_1PFYN73SzUXka-kBF79SZ0fDtE8H9Er652D3DNseJSmg2YVMbY9JeQqA; SID=DQAAAE8BAAAuFM32zsvEOFydoEWOaE2cqqnksFSUAT9DP375tWueV_hIS1rJAKLpxyTqMThk4iUstXKJAub8T-xhhptcCq2-HtsCpHAHdlS8N0pWqvrlnYG1rpLltqAJivex1U5MlIi5JIzDnufD9bp8JjLQnbpe1NO3N4Ya8BefLVEPlfKi3zUVs_AaMsmsF5h4_lhsz91LrRKi9Lb-qQhO3XOl9BHF9q7FHrkVQz6Q7E6a3Y5IchHUEdOHoiXaHWI_LQI9pHSSlVLlT1pgzhVLp-w7rgVaSlBQLu48hBP1XRGNiqGSQobB7g1zkJIdDWQRMAgoP9mtQr7fJ6YPDhQQn8DjSE_e3JzkafpVwoQBETU3wJMp7a7igXMiHbFPpczBa32qa5VL_ls5imhLHKFXFGKU7qzUI9amFKGlq6PMLWerghufnBbV_onfgT9dwMsxbVdzmBE; HSID=AMjtxpc0Ym2NlGxni; APISID=pO-iHi7nSk_X_-H9/AR7f9xYUMTKcNwv6z; GDSESS=ID=d5de0ada6ebb0180:TM=1366207841:C=c:IP=178.85.254.34-:S=APGng0vQoVEWoUe0cvIlLVGfMX4zD35pZg'
 	request = urllib2.Request(url)
-	request.add_header('User-agent', 'Mozilla/5.0') 
-	opener = urllib2.build_opener()
+	request.add_header('Host', 'translate.google.com')
+	request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0')
+	request.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+	request.add_header('Accept-Language', 'en-gb,en;q=0.5')
+	request.add_header('Accept-Encoding', 'gzip, deflate')
+	# request.add_header('Cookie', cookie)
+	request.add_header('Connection', 'keep-alive')
+	# opener = urllib2.build_opener()
+	
+	# With cookies
+	cj = cookielib.CookieJar()
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+	
 	
 	md5sum = util.string_to_md5(text)
 	file = md5sum + '.mp3'
-	if not (os.path.isfile(file)):
+	
+	try:
+		if os.stat(file).st_size <= 1024:
+			#file is smaller than 1kbyte and should be deleted
+			os.remove(file)
+	except Exception:
+		# Carry on, file probably doesn't exist
+		pass
+		
+	if not os.path.isfile(file):
 		# File doesn't exist yet
 		f = open(file, "wb")
-		f.write(opener.open(request).read())
+		try:
+			ttsResult = opener.open(request)
+			# Add delay to refrain from battering Google's servers
+			time.sleep(uniform(1, 4))
+		except urllib2.HTTPError as e:
+			# No internet or blocked from Google
+			return 'ERROR'
+		
+		if ttsResult:
+			f.write(ttsResult.read())
 		f.close()
+		
+		try:
+			if os.stat(file).st_size <= 1024:
+				# File is smaller than 256 bytes and should be deleted
+				os.remove(file)
+				return 'ERROR'
+		except Exception:
+			# File doesn't exist while it should! 
+			return 'ERROR'
 		
 	return file
 
@@ -154,7 +196,7 @@ def filegenerator_run(form):
 def filegenerator_preview(form):
 	return playGoogleTTS(unicode(form.texttoTTS.toPlainText()), slanguages[form.comboBoxGoogle.currentIndex()][0])
 
-DefaultGoogleVoice = get_language_id('en')
+DefaultGoogleVoice = get_language_id('zh')
 
 TTS_service = {'g' : {
 'name': 'Google',
@@ -165,6 +207,3 @@ TTS_service = {'g' : {
 'filegenerator_layout': filegenerator_layout,
 'filegenerator_preview': filegenerator_preview,
 'filegenerator_run': filegenerator_run}}
-
-
-
